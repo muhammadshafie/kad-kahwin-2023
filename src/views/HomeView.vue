e28f82ac9a85a077f58210fd4b32f103556a4868
Muhammad Shafie <muhammadshafie@Muhammads-MacBook-Air.local>
2024-08-29 03:16:19 +0800

    create homepage promoting this website services with CTA button

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,38 @@
 <script setup></script>
 
 <template>
-  <main>HomePage</main>
+    <div class="min-w-screen min-h-screen flex items-center justify-center bg-white text-gray-800 shadow-xl h-full">
+      <section id="hero" class="w-full h-full pb-24 bg-white text-black">
+            <BaseSection>
+              <div class="col-span-12 lg:col-span-6 mt-12 xl:mt-10 space-y-4 sm:space-y-6 px-6 text-center sm:text-left">
+                <span data-aos="fade-right" data-aos-once="true" class="text-black text-gradient font-semibold uppercase"
+                  >Dapatkan</span
+                >
+                <h1
+                  data-aos="fade-right"
+                  data-aos-once="true"
+                  class="text-[2.5rem] sm:text-5xl xl:text-6xl font-bold leading-tight capitalize sm:pr-8 xl:pr-10"
+                >
+                  Kad <span class="text-header-gradient">Kahwin</span> Paling Santai~
+                </h1>
+                <p data-aos="fade-down" data-aos-once="true" data-aos-delay="300" class="paragraph hidden sm:block">
+                  Perkhidmatan kad kahwin yg paling mudah untuk digunakan
+                </p>
+                <div
+                  data-aos="fade-up"
+                  data-aos-once="true"
+                  data-aos-delay="700"
+                  class="flex flex-col sm:flex-row space-y-4 sm:space-y-0 sm:space-x-4 mt-2"
+                >
+                  <a href="https://wa.link/kylvty" target="_blank"
+                    class="max-w-full cursor-pointer rounded-2xl px-8 py-4 bg-gradient-to-r from-[#468ef9] to-[#0c66ee] border border-[#0c66ee] text-white"
+                  >
+                    Hubungi Sekarang!
+                  </a>
+                </div>
+              </div>
+      
+            </BaseSection>
+          </section>
+    </div>
 </template>
